2ac618c228c0b2b4ad40f66e7292ea35f438d26a
prmstrgt <pramestiregitaa@gmail.com>
2021-02-10 19:04:18 +0700

    slicing LandingPage

--- src/screens/LandingPage/index.js
+++ src/screens/LandingPage/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import {SliderBox} from 'react-native-image-slider-box';
+import {Button} from 'native-base';
 
 import styles from './style';
 import color from '../../assets/color';
@@ -9,9 +10,9 @@ export default function LandingPage() {
   const [sliderWidth, setWidth] = useState(0);
   const images = [
     require('../../assets/img1.jpg'),
-    require('../../assets/img1.jpg'),
-    require('../../assets/img1.jpg'),
-    require('../../assets/img1.jpg'),
+    require('../../assets/img2.jpg'),
+    require('../../assets/img3.jpg'),
+    require('../../assets/img4.jpg'),
   ];
 
   const onLayout = (e) => {
@@ -20,14 +21,28 @@ export default function LandingPage() {
 
   return (
     <View style={styles.parent} onLayout={onLayout}>
-      <View style={styles.space} />
       <SliderBox
         images={images}
-        sliderBoxHeight={400}
+        sliderBoxHeight={700}
         parentWidth={sliderWidth}
         dotColor={color.dotColor}
         inactiveDotColor={color.inactiveDotColor}
+        ImageComponentStyle={styles.image}
+        resizeMethod={'resize'}
+        resizeMode={'center'}
+        disableOnPress={true}
       />
+      <View style={styles.bottomWrapper}>
+        <Button style={styles.signupBtn} block>
+          <Text style={styles.signupBtnText}>Daftar Sekarang</Text>
+        </Button>
+        <View style={styles.loginWrapper}>
+          <Text style={styles.text}>Sudah punya akun?</Text>
+          <TouchableOpacity style={styles.loginBtn}>
+            <Text style={styles.loginBtnText}>Masuk</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
--- src/screens/LandingPage/style.js
+++ src/screens/LandingPage/style.js
@@ -6,7 +6,38 @@ export default StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
-  space: {
-    height: 200,
+  image: {
+    width: '100%',
+    backgroundColor: 'white',
+  },
+  bottomWrapper: {
+    marginTop: 20,
+    padding: 10,
+  },
+  signupBtn: {
+    borderRadius: 10,
+    backgroundColor: color.buttonColor,
+    height: 50,
+  },
+  signupBtnText: {
+    color: '#fff',
+    fontSize: 12,
+  },
+  loginWrapper: {
+    marginTop: 20,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 12,
+  },
+  loginBtn: {
+    marginLeft: 5,
+  },
+  loginBtnText: {
+    fontSize: 12,
+    color: color.buttonColor,
   },
 });
