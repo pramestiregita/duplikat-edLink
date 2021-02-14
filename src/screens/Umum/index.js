22a60eaaba65ce372573fbb6b167cf8e8a25f970
prmstrgt <pramestiregitaa@gmail.com>
2021-02-14 23:54:01 +0700

    slicing Umum

--- src/screens/Umum/index.js
+++ src/screens/Umum/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, ImageBackground} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import styles from './style';
 
 export default function Umum() {
   return (
     <View style={styles.parent}>
-      <Text style={styles.title}>this page is not available</Text>
+      <TouchableOpacity style={styles.card}>
+        <ImageBackground
+          style={styles.image}
+          source={require('../../assets/classroom.jpg')}>
+          <View style={styles.wrapper}>
+            <View style={styles.cardHeader}>
+              <View style={styles.header}>
+                <Text style={styles.title}>Helpdesk edLink</Text>
+              </View>
+              <Icon name="ellipsis-v" size={15} color="#fff" />
+            </View>
+            <View style={styles.content}>
+              <Text style={styles.text}>
+                Update terakhir : Senin, 7 November 2016
+              </Text>
+            </View>
+          </View>
+        </ImageBackground>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/screens/Umum/style.js
+++ src/screens/Umum/style.js
@@ -4,12 +4,37 @@ export default StyleSheet.create({
   parent: {
     flex: 1,
     backgroundColor: '#ffff',
+  },
+  card: {
+    width: '100%',
+    height: 100,
+  },
+  wrapper: {
+    flex: 1,
     padding: 10,
-    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.35)',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
     justifyContent: 'center',
   },
+  cardHeader: {
+    flexDirection: 'row',
+  },
+  header: {
+    flex: 1,
+  },
   title: {
-    textTransform: 'uppercase',
-    fontSize: 16,
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#fff',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
   },
 });
